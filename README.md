03a14797d34d6d3b4e3f9328673c16a317b5da33
Christopher Zechendorf <christopher@zechendorf.com>
2016-07-27 08:55:10 -0400

    0.1.1 initializing

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This extension provides AMP functionality for TYPO3.
 
 Since AMP pages have to be very strictly implemented this extension will only support TextMedia Content Elements. Feel free to implement other content elements.
+
+
---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => '⚡ AMP - Accellerated Mobile Pages ',
+	'description' => 'AMP - Accellerated Mobile Pages for TYPO3 ',
+	'category' => 'plugin',
+	'author' => 'Christopher Zechendorf',
+	'author_email' => 'christopher@zechendorf.com',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => '1',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '0.1.1',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-7.6.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
