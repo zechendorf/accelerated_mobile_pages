b1c12c8d5b02d4017d620f9dc582985c86dad8e2
Christopher Zechendorf <christopher@zechendorf.com>
2016-07-29 19:07:26 +0200

    amp ad plugin

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,3 +2,10 @@
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+	'ZECHENDORF.' . $_EXTKEY,
+	'Ad',
+	array('Ad' => 'show'),
+	array()
+);
--- ext_tables.php
+++ ext_tables.php
@@ -3,4 +3,15 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	'ZECHENDORF.' . $_EXTKEY,
+	'Ad',
+	'Ad'
+);
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', '⚡ AMP');
+
+$pluginSignature = 'acceleratedmobilepages_ad';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue( $pluginSignature, 'FILE:EXT:accelerated_mobile_pages/Configuration/FlexForms/AmpAd.xml');
