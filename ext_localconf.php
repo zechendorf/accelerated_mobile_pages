ff61a0c38f807dfe987ccde86effb582bcde44d0
Christopher Zechendorf <christopher@zechendorf.com>
2017-06-16 14:47:53 +0200

    [TASK] PSR-2 code cleanup - fixes #10

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,11 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'ZECHENDORF.' . $_EXTKEY,
-	'Ad',
-	array('Ad' => 'show'),
-	array()
+    'ZECHENDORF.' . $_EXTKEY,
+    'Ad',
+    array('Ad' => 'show'),
+    array()
 );
